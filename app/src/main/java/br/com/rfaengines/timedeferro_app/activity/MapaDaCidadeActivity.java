9261abd938b7748d37ccd21ec2138c008edbf289
Roger de Azevedo <rogerfdeazevedo@gmail.com>
2021-12-08 15:01:59 -0300

    tela 03 - add icones das missoes

--- app/src/main/java/br/com/rfaengines/timedeferro_app/activity/MapaDaCidadeActivity.java
+++ app/src/main/java/br/com/rfaengines/timedeferro_app/activity/MapaDaCidadeActivity.java
@@ -1,6 +1,11 @@
 package br.com.rfaengines.timedeferro_app.activity;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,10 +13,24 @@ import br.com.rfaengines.timedeferro_app.R;
 
 public class MapaDaCidadeActivity extends AppCompatActivity {
 
+    private ImageView imgView_Lvl1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa_da_cidade);
         getSupportActionBar().hide();
+
+        imgView_Lvl1 = findViewById(R.id.imgView_Lvl1);
+
+        imgView_Lvl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(MapaDaCidadeActivity.this, ResumoDaTretaActivity.class);
+                //startActivity(intent);
+                Toast.makeText(MapaDaCidadeActivity.this, "Quase pronto!", LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
