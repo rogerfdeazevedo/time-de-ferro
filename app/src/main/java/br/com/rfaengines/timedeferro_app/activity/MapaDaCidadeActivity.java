7863c23abcd4439022c319f0213e87850434e12e
Roger de Azevedo <rogerfdeazevedo@gmail.com>
2021-12-14 00:14:01 -0300

    revisao mapa da cidade activity

--- app/src/main/java/br/com/rfaengines/timedeferro_app/activity/MapaDaCidadeActivity.java
+++ app/src/main/java/br/com/rfaengines/timedeferro_app/activity/MapaDaCidadeActivity.java
@@ -9,13 +9,20 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.rfaengines.timedeferro_app.R;
+import br.com.rfaengines.timedeferro_app.gameplay.GamePlay;
 import br.com.rfaengines.timedeferro_app.gameplay.GamePlayManager;
 
 public class MapaDaCidadeActivity extends AppCompatActivity {
 
-    private ImageView imgView_Lvl1;
+    private TextView txtView_tentativas;
 
-    private TextView txtView_PlayerStatus;
+    private ImageView imgView_Lvl1;
+    private ImageView imgView_Lvl2;
+    private ImageView imgView_Lvl3;
+    private ImageView imgView_Lvl4;
+    private ImageView imgView_Lvl5;
+
+    private GamePlay gamePlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +31,7 @@ public class MapaDaCidadeActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         carregarComponentes();
-        carregarDadosUi();
+        carregadorDadosUI();
 
         imgView_Lvl1.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -36,13 +43,66 @@ public class MapaDaCidadeActivity extends AppCompatActivity {
     }
 
     private void carregarComponentes(){
+
+        txtView_tentativas = findViewById(R.id.txtView_tentativas);
+
         imgView_Lvl1 = findViewById(R.id.imgView_Lvl1);
-        txtView_PlayerStatus = findViewById(R.id.txtView_PlayerStatus);
+        imgView_Lvl2 = findViewById(R.id.imgView_Lvl2);
+        imgView_Lvl3 = findViewById(R.id.imgView_Lvl3);
+        imgView_Lvl4 = findViewById(R.id.imgView_Lvl4);
+        imgView_Lvl5 = findViewById(R.id.imgView_Lvl5);
+
+        gamePlay = GamePlayManager.getGamePlay();
     }
 
-    private void carregarDadosUi(){
-        String qtdContinues = txtView_PlayerStatus.getText() + " " + GamePlayManager.getGamePlay().getJogador().getQtdContinue();
-        txtView_PlayerStatus.setText(qtdContinues );
+    private void carregadorDadosUI(){
+        carregarStatusJogador();
+        controleDeVisibilidadeDeLevel();
+    }
+
+    private void carregarStatusJogador(){
+        String qtdStr = "" + gamePlay.getJogador().getQtdContinue();
+        String tentativas = getString(R.string.game_continue, qtdStr);
+        txtView_tentativas.setText(tentativas);
+    }
+
+    private void controleDeVisibilidadeDeLevel(){
+
+         if(gamePlay.getLevelAtual()==1){
+            imgView_Lvl2.setVisibility(View.INVISIBLE);
+            imgView_Lvl3.setVisibility(View.INVISIBLE);
+            imgView_Lvl4.setVisibility(View.INVISIBLE);
+            imgView_Lvl5.setVisibility(View.INVISIBLE);
+        }
+
+        if(gamePlay.getLevelAtual()==2){
+            imgView_Lvl2.setVisibility(View.VISIBLE);
+            imgView_Lvl3.setVisibility(View.INVISIBLE);
+            imgView_Lvl4.setVisibility(View.INVISIBLE);
+            imgView_Lvl5.setVisibility(View.INVISIBLE);
+        }
+
+        if(gamePlay.getLevelAtual()==3){
+            imgView_Lvl2.setVisibility(View.VISIBLE);
+            imgView_Lvl3.setVisibility(View.VISIBLE);
+            imgView_Lvl4.setVisibility(View.INVISIBLE);
+            imgView_Lvl5.setVisibility(View.INVISIBLE);
+        }
+
+        if(gamePlay.getLevelAtual()==4){
+            imgView_Lvl2.setVisibility(View.VISIBLE);
+            imgView_Lvl3.setVisibility(View.VISIBLE);
+            imgView_Lvl4.setVisibility(View.VISIBLE);
+            imgView_Lvl5.setVisibility(View.INVISIBLE);
+        }
+
+        if(gamePlay.getLevelAtual()==5){
+            imgView_Lvl2.setVisibility(View.VISIBLE);
+            imgView_Lvl3.setVisibility(View.VISIBLE);
+            imgView_Lvl4.setVisibility(View.VISIBLE);
+            imgView_Lvl5.setVisibility(View.VISIBLE);
+        }
+
     }
 
 }
